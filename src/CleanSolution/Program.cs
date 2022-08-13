f4ae040e18bf08c39fbe182c549295405652dfcb
MSPro <Markus@MarkusSchmidt.PRO>
2022-08-13 12:11:10 +0200

    Use NLog as alternative Logger

--- src/CleanSolution/Program.cs
+++ src/CleanSolution/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.IO;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using MSPro.CLArgs;
+using NLog.Extensions.Logging;
 
 
 
@@ -11,6 +14,16 @@ builder.ConfigureCommands(commands =>
 {
     commands.AddAssemblies(new AssemblyCommandResolver2(
                                Directory.GetFiles(Environment.CurrentDirectory, SEARCH_PATTERN, SearchOption.AllDirectories)));
+});
+builder.ConfigureServices((services, settings) =>
+{
+    services.AddLogging(loggingBuilder =>
+    {
+        // By default CLArgs uses .NET Console logger for logging. You may override this and
+        // provide your own logging provider, e.g. NLog
+        loggingBuilder.ClearProviders();
+        loggingBuilder.AddNLog();
+    });
 });
 Commander2 commander = builder.Build();
 commander.Execute();
