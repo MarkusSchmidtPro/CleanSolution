137bdc7c9e89fd892e62113b03270b368bb15c2d
MSPro <Markus@MarkusSchmidt.Pro>
2025-10-10 11:13:08 +0200

    Merged and updated to SLNX

--- src/CleanSolution.Command/Services/ParseDirectory.cs
+++ src/CleanSolution.Command/Services/ParseDirectory.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
-<<<<<<< HEAD
-=======
 using CleanSolution.Services;
->>>>>>> dev
 
 
 
